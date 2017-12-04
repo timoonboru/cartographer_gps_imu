0e8b9bc13bd1a7f9577d25587471c417a6cb6353
timoonboru <584778678@qq.com>
2017-12-04 14:30:03 +0800

    I change master

--- cartographer_ros/cartographer_ros/cartographer_ros/node.cc
+++ cartographer_ros/cartographer_ros/cartographer_ros/node.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// I change with master
 #include "cartographer_ros/node.h"
 
 #include <chrono>
